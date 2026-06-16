64ed6f925c900cadb3b489d01057b304c203f2d2
jerichoalmiranez-sketch <jerichoalmiranez@gmail.com>
2026-06-15 22:09:19 -0700

    Update page.tsx

--- app/world-cup/[date]/page.tsx
+++ app/world-cup/[date]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { collection, onSnapshot } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "../../../lib/firebase";
 
 export default function Page({ params }: { params: { date: string } }) {
   const [matches, setMatches] = useState<any[]>([]);
